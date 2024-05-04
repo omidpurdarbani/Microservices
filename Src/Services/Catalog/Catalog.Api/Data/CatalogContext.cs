90e1918d2b9bed7d04b9deb6d7eec02ccaf90a79
Omid purdarbani <omidpurdarbani@gmail.com>
2024-05-04 16:22:21 +0330

    commit

---
+++ Src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+            CatalogDbSeeder.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ Src/Services/Catalog/Catalog.Api/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Api.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public required string Name { get; set; }
+
+        public required string Category { get; set; }
+
+        public required string Summary { get; set; }
+
+        public required string Description { get; set; }
+
+        public required string ImageFile { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
